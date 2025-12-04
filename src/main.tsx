ae1b1ccfb5a25ff44ea4f6bb223efa36945c73c7
julasweta <julasweta@ukr.net>
2025-12-05 00:26:57 +0200

    path3

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <HashRouter basename="/auth">
+    <HashRouter basename="/">
 
       <App />
 
