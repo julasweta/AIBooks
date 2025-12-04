4ed1922b67f0562681fcb4cd6f07d7a78fedad1e
julasweta <julasweta@ukr.net>
2025-12-05 00:21:31 +0200

    Configure Vite for Vercel + BrowserRouter

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/auth',          // якщо сайт на root-домені Vercel
+  build: {
+    outDir: 'dist',   // директорія збірки Vite (Vercel її використовує)
+  },
 })
+
