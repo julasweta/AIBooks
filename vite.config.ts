0f23a287234f6c7f3d1d576a46f4c87313af3586
julasweta <julasweta@ukr.net>
2025-12-05 00:23:57 +0200

    path2

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/auth',          // якщо сайт на root-домені Vercel
+  base: '/',          // якщо сайт на root-домені Vercel
   build: {
     outDir: 'dist',   // директорія збірки Vite (Vercel її використовує)
   },
